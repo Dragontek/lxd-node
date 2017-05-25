05fa869205d833cc20037c2506c362b10ca0d6ba
Joshua Mondragon <jmondragon@saphira.dragontek.com>
2017-05-24 20:21:58 -0600

    Remove cli for now

--- pm-list.js
+++
@@ -1,7 +0,0 @@
-var ContainerServer = require('./classes/ContainerServer');
-
-var server = new ContainerServer('https://node1.dragontek.com:8443');
-
-server.GetContainers().then(function(containers) {
-    console.log(containers);
-});
