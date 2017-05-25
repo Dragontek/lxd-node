eb939d87eb3d8b7834fefe4e6e1ed133812c81c3
Joshua Mondragon <jmondragon@saphira.dragontek.com>
2017-05-24 20:22:46 -0600

    Fix json calls

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+var fs = require('fs'); 
+var cert = fs.readFileSync('client.crt');
+var key = fs.readFileSync('client.key');
+
 var ContainerServer = require('./lib/ContainerServer');
+var ImageServer = require('./lib/ImageServer');
 
-var server = new ContainerServer('https://node1.dragontek.com:8443');
+var server = new ContainerServer('https://tiamat.dragontek.com:8443', cert, key);
+var images = new ImageServer('https://us.images.linuxcontainers.org')
 
-server.GetCertificates().then(function(containers) {
-    console.log(containers);
+var container = {
+    "name": "my-new-container",
+    "architecture": "x86_64",
+    "profiles": ["default"],
+    "source": {"type": "image", "alias": "debian/latest"}
+}
+server.CreateContainer(container).then(function(result) {
+    console.log(result);
+}, function(error) {
+    console.error(error);
 });
